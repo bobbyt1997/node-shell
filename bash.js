34221c9ac4f143b32f90b82811f048fcb91eeb88
aidan_jabbarzadeh <jabbarz2@illinois.edu>
2020-06-19 11:45:24 -0500

    added pwd file and exported

--- bash.js
+++ bash.js
@@ -4,8 +4,7 @@ process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
 
   if (cmd === "pwd") {
-    console.log(__dirname);
+    const pwdFunc = require("./pwd.js");
+    pwdFunc();
   }
-
-  process.stdout.write("\nprompt > ");
 });
