00cc7457960b195c8275ef49493e912171d69e91
Bobby Ton <bobbyt1997@gmail.com>
2020-06-19 11:35:59 -0500

    complete pwd

---
+++ bash.js
@@ -0,0 +1,11 @@
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") {
+    console.log(__dirname);
+  }
+
+  process.stdout.write("\nprompt > ");
+});
